5955cc8d58e8fb3e1367d27413d699eae9ee9df4
George Sofroniou | Delfi Partners <gs@delfipartners.com>
2024-10-31 14:55:37 +0200

    testing

---
+++ tests/Feature/FetchStockPricesCommandTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Tests\TestCase;
+
+class FetchStockPricesCommandTest extends TestCase
+{
+//    use RefreshDatabase;
+
+    public function test_fetches_stock_prices_and_updates_the_database(): void
+    {
+
+    }
+}
